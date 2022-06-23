649283e1593a0b2b7bb829a03b1ef6bacbe0152d
Lucas Paixao <lucasspxo@gmail.com>
2022-06-23 16:29:12 -0300

    firebase

--- src/main/kotlin/io/github/xuenqui/eventosdarep/application/factories/FirebaseFactory.kt
+++ src/main/kotlin/io/github/xuenqui/eventosdarep/application/factories/FirebaseFactory.kt
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import io.github.xuenqui.eventosdarep.logging.LoggableClass
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.ConfigurationProperties
 import io.micronaut.context.annotation.Factory
@@ -20,6 +21,7 @@ class FirebaseFactory(
     @Singleton
     fun firebaseMessaging(): FirebaseMessaging {
         val loader: ClassPathResourceLoader = ResourceResolver().getLoader(ClassPathResourceLoader::class.java).get()
+        logger.info("loading firebase config: ${firebaseProperty.path}")
         val resource = loader.getResource(firebaseProperty.path)
         val googleCredentials = GoogleCredentials
             .fromStream(resource.get().openStream())
@@ -29,9 +31,11 @@ class FirebaseFactory(
             .setCredentials(googleCredentials)
             .build()
 
-        val app = FirebaseApp.initializeApp(firebaseOptions, "eventos-da-rep")
+        val app = FirebaseApp.initializeApp(firebaseOptions)
         return FirebaseMessaging.getInstance(app)
     }
+
+    companion object : LoggableClass()
 }
 
 @ConfigurationProperties("firebase")
