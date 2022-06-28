321c25d53ae556d85bd5e8c2c8d2ced6dc6ead1e
Lucas Paixao <lucasspxo@gmail.com>
2022-06-28 17:14:40 -0300

    fix priority

--- src/main/kotlin/io/github/xuenqui/eventosdarep/resources/firebase/FirebaseMessagingService.kt
+++ src/main/kotlin/io/github/xuenqui/eventosdarep/resources/firebase/FirebaseMessagingService.kt
@@ -1,5 +1,7 @@
 package io.github.xuenqui.eventosdarep.resources.firebase
 
+import com.google.firebase.messaging.AndroidConfig
+import com.google.firebase.messaging.ApnsConfig
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.Notification
@@ -16,9 +18,14 @@ class FirebaseMessagingService(
             .setBody(messasge)
             .build()
 
+        val androidConfig = AndroidConfig.builder().setPriority(AndroidConfig.Priority.HIGH).build()
+        val apnConfig = ApnsConfig.builder().putHeader("apns-priority", "10").build()
+
         val message = Message.builder()
             .setTopic(topic)
             .setNotification(notification)
+            .setAndroidConfig(androidConfig)
+            .setApnsConfig(apnConfig)
             .build()
 
         logger.info("Sending notification to topic $topic with the title $title and body $message")
